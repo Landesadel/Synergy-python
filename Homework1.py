8f8f7fc127444a41ead090d544f80119d60d9476
Landesadel <iliillimusicforever@gmail.com>
2024-01-29 18:08:20 +0300

    added ex1 and ex2

---
+++ Homework1.py
@@ -0,0 +1,18 @@
+#Ex1
+petType = input("Введите тип питомца: ")
+petName = input("Введите имя питомца: ")
+petAge = input("Введите возраст питомца: ")
+
+print(f"Это {petType} по кличке \"{petName}\". Возраст: {petAge} года")
+
+#Ex2
+print("Введите последовательно названия этапов развития человека ")
+
+firstStage = input("Первый этап: ")
+secondStage = input("Второй этап: ")
+thirdStage = input("Третий этап: ")
+fourthStage = input("Четвертый этап: ")
+fifthStage = input("Пятый этап: ")
+sixthStage = input("Шестой этап: ")
+
+print(fifthStage, secondStage, thirdStage, fourthStage, fifthStage, sixthStage, sep='=>')
